6b03253e3c87f089d0cc2ccab63a91d006177092
yangen <yang_sen_ysys@163.com>
2018-02-24 22:47:08 +0800

    First

---
+++ Main/Main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by yangsen on 2018/2/24.
+ */
+
+headerTitle = new Vue({
+    el: '.headTitleView',
+    data: {
+        headTitle: '杨稼轩'
+    }
+})
+
+titles = new Vue({
+    el: '.leftTitlesView',
+    data: {
+        titles: [
+            {title: "项目一"},
+            {title: "项目二"},
+            {title: "项目三"},
+            {title: "项目四"},
+            {title: "项目五"},
+            {title: "项目六"}
+        ]
+    }
+})
