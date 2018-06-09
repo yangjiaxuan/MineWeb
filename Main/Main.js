7c9eb8c36bd2dc2d752c5c4d04e2c21a5578da58
yangen <yang_sen_ysys@163.com>
2018-06-09 15:05:52 +0800

    Test

--- Main/Main.js
+++ Main/Main.js
@@ -14,18 +14,47 @@ titles = new Vue({
     data: {
         name : "10",
         infos: [
-            {title: "项目一", action:"http://www.baidu.com"},
-            {title: "项目二", action:"https://yangjiaxuan.github.io/Main/Main"},
-            {title: "项目三", action:"https://yangjiaxuan.github.io/../MyNote/blob/master/JNI.md"},
-            {title: "项目四", action:"https://github.com/yangjiaxuan/MyNote/blob/master/JNI.md"},
-            {title: "项目五", action:"5"},
-            {title: "项目六", action:"6"}
+            {title: "项目一", type:"web", action:"http://www.baidu.com"},
+            {title: "项目二", type:"web", action:"https://yangjiaxuan.github.io"},
+            {title: "项目三", type:".md", action:"./MyNote/JNI.md"},
+            {title: "项目四", type:".md", action:"https://github.com/yangjiaxuan/MyNote/JNI.md"},
+            {title: "项目五", type:"web", action:"https://yangjiaxuan.github.io/index.html"},
+            {title: "项目六", type:"", action:"6"}
         ]
     },
     methods : {
         titleOnClick:function(info) {
-            window.open(info.action);
-        }
+            if(info.type == "web"){
+                this.goToOtherWind(info.action);
+            }else if(info.type == ".md"){
+                this.getInfoContent(info.action);
+            }
+        },
+        goToOtherWind:function (url) {
+            window.open(url)
 
-    }
+        },
+        getInfoContent:function(url){
+
+            var xmlhttp;
+            if (window.XMLHttpRequest)
+            {// code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            }else {// code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.open("GET",url);
+            xmlhttp.send(null);
+
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    var rightView=document.getElementsByClassName("rightView")[0];
+                    rightView.innerHTML=marked(xmlhttp.responseText);
+                }
+                else {
+                    alert(xmlhttp.statusText);
+                }
+            }
+        }
+}
 });
