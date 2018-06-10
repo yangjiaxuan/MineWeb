0b552d024932627f63d5401174cbe88f1c7173e0
yangen <yang_sen_ysys@163.com>
2018-06-10 19:49:33 +0800

    modify style

--- Main/Main.js
+++ Main/Main.js
@@ -10,18 +10,20 @@ headerTitle = new Vue({
 });
 
 titles = new Vue({
+
+    http: {
+        root: '/root',
+        headers: {
+            Authorization: ''
+        }
+    },
     el: '.leftTitlesView',
     data: {
-        name : "10",
-        infos: [
-            {title: "项目一", type:"web", action:"http://www.baidu.com"},
-            {title: "项目二", type:"web", action:"https://yangjiaxuan.github.io"},
-            {title: "项目三", type:".md", action:"./MyNote/JNI.md"},
-            {title: "项目四", type:".md", action:"https://github.com/yangjiaxuan/MyNote/JNI.md"},
-            {title: "项目五", type:"web", action:"https://yangjiaxuan.github.io/index.html"},
-            {title: "项目六", type:"", action:"6"}
-        ]
-    },
+        name : "baudu",
+        isShowInfos : false,
+        infos:[]
+        // "{'title': '', 'type':'.md', 'action':'https://github.com/yangjiaxuan/MyNote/JNI.md'}"
+        },
     methods : {
         titleOnClick:function(info) {
             if(info.type == "web"){
@@ -53,5 +55,28 @@ titles = new Vue({
                 }
             }
         }
-}
+    }
 });
+
+function loadData() {
+
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }else {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET","./Resource/AppData.json");
+    xmlhttp.send(null);
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var result = JSON.parse(xmlhttp.responseText);
+            // titles.infos.push(result['infos']);
+            // Array.prototype.push.apply(titles.infos, result.infos);
+            titles.infos = titles.infos.concat(result.infos);
+            titles.isShowInfos = true;
+        }
+    }
+}
